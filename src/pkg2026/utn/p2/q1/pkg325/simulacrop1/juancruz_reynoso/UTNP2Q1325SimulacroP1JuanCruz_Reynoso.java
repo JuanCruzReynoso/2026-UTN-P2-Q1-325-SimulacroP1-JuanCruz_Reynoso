513cdc6433e1ff460a1df5d04fda53bbdb8b32dc
JuanCruzReynoso <juanosk898@gmail.com>
2026-05-19 19:12:21 -0300

    feat(docs): inicializar proyecto y detallar README.md

---
+++ src/pkg2026/utn/p2/q1/pkg325/simulacrop1/juancruz_reynoso/UTNP2Q1325SimulacroP1JuanCruz_Reynoso.java
@@ -0,0 +1,16 @@
+package pkg2026.utn.p2.q1.pkg325.simulacrop1.juancruz_reynoso;
+
+/**
+ *
+ * @author Juan Cruz Reynoso
+ */
+public class UTNP2Q1325SimulacroP1JuanCruz_Reynoso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
